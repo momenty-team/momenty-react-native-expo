5450319400071c7f2015e945030469eac9a5ab29
hyejun <hyejunkkim228@gmail.com>
2025-01-23 15:48:18 +0900

    Initial commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+import {View, Text} from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>모먼티</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    padding: 16,
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 24,
+    textAlign: 'center',
+    marginBottom: 16,
+  },
+});
