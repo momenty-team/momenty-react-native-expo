e1f274555d37bf57971e487beba00334dc6e3395
d0dam <smallkdb@gmail.com>
2025-02-07 16:11:32 +0900

    refactor: header option off

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
     </Stack>
