2f566eda89d859eea16406ee2924988df5ca362c
hyejun <hyejunkkim228@gmail.com>
2025-02-10 17:40:31 +0900

    feat: 캘린더 진입점 구현

--- app/calendar/index.tsx
+++ app/calendar/index.tsx
@@ -6,9 +6,11 @@ export default function WebViewScreen() {
   const insets = useSafeAreaInsets();
 
   return (
-    <WebView
-      source={{ uri: 'http://172.19.83.8:3000/add-log' }}
-      style={{ flex: 1, paddingTop: insets.top, backgroundColor: '#F4F6F9' }}
-    />
+    <>
+      <WebView
+        source={{ uri: 'http://172.19.87.2:3000/calendar' }}
+        style={{ flex: 1, paddingTop: insets.top, backgroundColor: '#F4F6F9' }}
+      />
+    </>
   );
 }
