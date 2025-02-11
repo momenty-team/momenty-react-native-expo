e111262f8ae9384daf3033264a4fc4f7ff70dc89
d0dam <smallkdb@gmail.com>
2025-02-11 15:03:38 +0900

    feat: injection template 함수 제작

--- app/(tabs)/user.tsx
+++ app/(tabs)/user.tsx
@@ -1,42 +1,9 @@
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { useEffect, useState } from 'react';
-import { WEBVIEW_BASE_URL } from '@/constants/environment';
-import WebView from 'react-native-webview';
+import UserPage from '@/app/user';
 
 function User() {
-  const insets = useSafeAreaInsets();
-  const [notchHeight, setNotchHeight] = useState(0);
-
-  useEffect(() => {
-    setNotchHeight(insets.top);
-  }, [insets]);
-
-  const injectedCSS = `
-    const style = document.createElement('style');
-    style.innerHTML = \`
-      body {
-        padding-top: ${notchHeight}px !important;
-      }
-
-      * {
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-        -webkit-touch-callout: none;
-      }
-    \`;
-    document.head.appendChild(style);
-    true;
-`;
 
   return (
-    <WebView
-      source={{ uri: `${WEBVIEW_BASE_URL}/user` }}
-      injectedJavaScript={injectedCSS}
-      onMessage={(_) => {}}
-      style={{ flex: 1 }}
-    />
+    <UserPage />
   );
 }
 
