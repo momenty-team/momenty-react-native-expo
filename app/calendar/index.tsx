900764e5bdc9f6520c5758e4b3d74c4e39d36e57
hyejun <hyejunkkim228@gmail.com>
2025-02-11 09:52:55 +0900

    refactor: 불필요한 코드 삭제

--- app/calendar/index.tsx
+++ app/calendar/index.tsx
@@ -6,11 +6,9 @@ export default function WebViewScreen() {
   const insets = useSafeAreaInsets();
 
   return (
-    <>
       <WebView
-        source={{ uri: 'http://172.19.87.2:3000/calendar' }}
+        source={{ uri: 'http://192.168.50.144:3000/calendar' }}
         style={{ flex: 1, paddingTop: insets.top, backgroundColor: '#F4F6F9' }}
       />
-    </>
   );
 }
